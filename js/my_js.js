bb473b6dfbfeff8e1bcbb8a0b39b8701c12234ca
Kira <lutra-lo@ya.ru>
2018-06-14 03:10:29 +0300

    First draft

---
+++ js/my_js.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('#let_kira').each(function (i) {
+        var name = "lutra007";
+        var host = "gmail.com";
+        document.getElementById('let_kira').innerHTML = name + "@" + host;
+    });
+});
